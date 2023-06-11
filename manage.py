8697967eb084dea2d66267d487a3018226331b9f
piotrzegarek <92168516+piotrzegarek@users.noreply.github.com>
2023-06-12 00:07:36 +0200

    create db app route

--- manage.py
+++ manage.py
@@ -4,11 +4,12 @@ from app.models import db
 app = create_app()
 
 @app.route("/create_db")
-@app.cli.command("create_db")
+#@app.cli.command("create_db")
 def create_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+    return "DB created"
 
 if __name__ == "__main__":
     sio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
